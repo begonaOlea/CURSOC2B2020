29f90feeac68e84435cf1da145bb7b86986305c2
begonaolea <begonaolea@192.168.1.8>
2020-11-19 09:49:59 +0100

    COLLECTIONS

---
+++ src/main/java/modulo9/collection/PruebaAPICollections.java
@@ -0,0 +1,21 @@
+
+package modulo9.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author begonaolea
+ */
+public class PruebaAPICollections {
+    public static void main(String[] args) {
+        
+        ArrayList<Alumno> lista = new ArrayList<Alumno>();
+        
+        //Collections.sort
+        
+        
+    }//fin main
+    
+}
