1f128d7662b6bd7db74004608697dd0b366b7730
begonaolea <begonaolea@192.168.1.8>
2020-11-20 10:31:49 +0100

    escribir fichero

--- src/main/java/modulo12/PruebaFicheros.java
+++ src/main/java/modulo12/PruebaFicheros.java
@@ -1,33 +1,56 @@
 package modulo12;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.PrintWriter;
 
 public class PruebaFicheros {
-    
+
     public static void main(String[] args) throws IOException {
-        
-        File dir = new File ("dir1");
-         
-        if(! dir.exists() ){
+
+        File dir = new File("dir1");
+
+        if (!dir.exists()) {
             System.out.println("dir 1 no existe. Creo el directorio");
             dir.mkdir();
-        }else{
+        } else {
             System.out.printf(" el directorio %s ya existe", dir.getName());
         }
-        
+
         File fichero = new File(dir, "carta.txt");
-        
-        if(! fichero.exists()){
+
+        if (!fichero.exists()) {
             System.out.println("creo el fichero");
             fichero.createNewFile();
-        }else{
+        } else {
             System.out.println("El fichero ya existe");
         }
         System.out.printf("Ruta parcial %s y ruta completa %s",
                 fichero.getPath(),
                 fichero.getAbsolutePath());
+
+        //ESCRIBIR EN EL FICHERO
+        FileWriter fw = null;
+        PrintWriter out = null;
+        try {
+            fw = new FileWriter(fichero,true);
+             out = new PrintWriter(fw);
+            
+            out.println("Esto es una linea. ");
+            out.println("Esto es otra linea. ") ;      
+//            fw.write("Esto es una primera línea.");
+//            fw.write("\n");
+//            fw.write("Esto es una segunda línea.");
+//            fw.write("\n");
+            
+        } catch (IOException e) {
+             System.out.println("No se ha podido escribir");
+        } finally {
+           if (fw != null) fw.close();
+           if (out != null) out.close();
+        }
+
     }//fin main 
-    
+
 }
