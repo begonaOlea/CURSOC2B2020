fbab6d9f9b76ab2edba86a81da1dc7ea49b459cd
begonaolea <begonaolea@192.168.1.8>
2020-11-25 12:25:52 +0100

    API JDBC

--- src/main/java/com/shop/PruebaAPIJDBC.java
+++ src/main/java/com/shop/PruebaAPIJDBC.java
@@ -15,6 +15,12 @@ public class PruebaAPIJDBC {
                Connection
          3. Obtener una sentencia para lanzar una instruccion
                Statement
+         4. Ejecutar sentencia generalista - execute
+            Ejecutar sentencia cambia algo  - executeUpdate 
+                   que retorna el número de reg afectados
+         5. Consultar todos cafés
+            Ejecutamos la sentencia SELEC - executeQuery
+                    que retorna un ResulSet  (resultado en forma de tabla)
         */
         
         try {
@@ -41,11 +47,34 @@ public class PruebaAPIJDBC {
             
             Statement sentencia = con.createStatement();
             //ejecuto
-            sentencia.execute(create);
+           // sentencia.execute(create);
             
+            //insertar datos en la tabla COFFEES
             
+            String poblarTabla = "INSERT INTO COFFEES " +
+            "VALUES ('Colombian', 101, 7.99, 0, 0),"
+                + "('French_Roast', 49, 8.99, 0, 0), "
+                + "('Espresso', 150, 9.99, 0, 0), "
+                + "('Colombian_Decaf', 101, 8.99, 0, 0), "
+                + "('French_Roast_Decaf', 49, 9.99, 0, 0)";
             
+            int filasInsertadas = sentencia.executeUpdate(poblarTabla);
+            System.out.printf("Ha insertado %d cafés.", filasInsertadas);
+  
+            //consultar los cafés de la tabla
+            String consultaCafes = "SELECT * FROM COFFEES";
             
+            ResultSet resultado = sentencia.executeQuery(consultaCafes);
+            //boolean hay = resultado.next(); 
+            //if(hay){
+            while(resultado.next()){
+                System.out.printf("café %s del proveedor id %d ",
+                         resultado.getString(1), // columan 1
+                         resultado.getInt(2)     //columna 2
+                         );
+            }//fin recorrer resultados
+           
+           
         } catch (ClassNotFoundException e){
             System.out.println("Error carga Driver: " + e.getMessage() );
         } catch (SQLException ex) {
