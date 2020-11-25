9d0b262258b3349f6a593e4c9d0a85344f717b22
begonaolea <begonaolea@192.168.1.8>
2020-11-25 12:58:07 +0100

    API JDBC

--- src/main/java/com/shop/PruebaAPIJDBC.java
+++ src/main/java/com/shop/PruebaAPIJDBC.java
@@ -5,9 +5,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PruebaAPIJDBC {
-    
+
     public static void main(String[] args) {
-        
+
         /*
          1. Cargar Driver  base de datos JavaDB (Derby)
                DriverManager
@@ -21,49 +21,48 @@ public class PruebaAPIJDBC {
          5. Consultar todos cafés
             Ejecutamos la sentencia SELEC - executeQuery
                     que retorna un ResulSet  (resultado en forma de tabla)
-        */
-        
+         */
+        Connection con = null;
         try {
             System.out.println("Intentando cargar driver");
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             System.out.println("Cargo ok el driver");
-            
+
             //conectar con la bd CoffeeShop
             String url = "jdbc:derby://localhost:1527/CoffeeShop";
             //usuario y clave no tiene
             System.out.println("... obtener conexión con la bd");
-            
-            Connection con = DriverManager.getConnection(url);
-            
+
+            con = DriverManager.getConnection(url);
+
             System.out.println(".... ok conexión");
-            
+
             //crear la tabla 
-            String create = "CREATE TABLE COFFEES " +
-            "           (COF_NAME VARCHAR(32), " +
-            "      SUP_ID INTEGER, " +
-            "      PRICE FLOAT, " +
-            "      SALES INTEGER, " +
-            "      TOTAL INTEGER)";
-            
+            String create = "CREATE TABLE COFFEES "
+                    + "           (COF_NAME VARCHAR(32), "
+                    + "      SUP_ID INTEGER, "
+                    + "      PRICE FLOAT, "
+                    + "      SALES INTEGER, "
+                    + "      TOTAL INTEGER)";
+
             Statement sentencia = con.createStatement();
             //ejecuto
-           // sentencia.execute(create);
-            
+            // sentencia.execute(create);
+
             //insertar datos en la tabla COFFEES
-            
-            String poblarTabla = "INSERT INTO COFFEES " +
-            "VALUES ('Colombian', 101, 7.99, 0, 0),"
-                + "('French_Roast', 49, 8.99, 0, 0), "
-                + "('Espresso', 150, 9.99, 0, 0), "
-                + "('Colombian_Decaf', 101, 8.99, 0, 0), "
-                + "('French_Roast_Decaf', 49, 9.99, 0, 0)";
-            
-         //   int filasInsertadas = sentencia.executeUpdate(poblarTabla);
-          //  System.out.printf("Ha insertado %d cafés.", filasInsertadas);
-  
+            String poblarTabla = "INSERT INTO COFFEES "
+                    + "VALUES ('Colombian', 101, 7.99, 0, 0),"
+                    + "('French_Roast', 49, 8.99, 0, 0), "
+                    + "('Espresso', 150, 9.99, 0, 0), "
+                    + "('Colombian_Decaf', 101, 8.99, 0, 0), "
+                    + "('French_Roast_Decaf', 49, 9.99, 0, 0)";
+
+           // int filasInsertadas = sentencia.executeUpdate(poblarTabla);
+           // System.out.printf("Ha insertado %d cafés.", filasInsertadas);
+
             //consultar los cafés de la tabla
             String consultaCafes = "SELECT * FROM COFFEES";
-            
+
             ResultSet resultado = sentencia.executeQuery(consultaCafes);
             //boolean hay = resultado.next(); 
             //if(hay){
@@ -78,14 +77,18 @@ public class PruebaAPIJDBC {
                 System.out.println("");
             }//fin recorrer resultados
 
-           
-        } catch (ClassNotFoundException e){
-            System.out.println("Error carga Driver: " + e.getMessage() );
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error carga Driver: " + e.getMessage());
         } catch (SQLException ex) {
             System.out.println("Error DB: " + ex.getMessage());
+        }finally{
+            if(con != null) try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(PruebaAPIJDBC.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        
-        
+
     }//fin main
-    
+
 }//fin class
