df544fa12d52df7be871ea3bb26e17ea42c4c361
begonaolea <begonaolea@192.168.1.8>
2020-11-26 13:26:18 +0100

    Pool uso en informes cafe

--- src/main/java/com/shop/GestionarCafes.java
+++ src/main/java/com/shop/GestionarCafes.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 
 public class GestionarCafes {
     
@@ -15,7 +16,7 @@ public class GestionarCafes {
                  " SET SALES = " + ventas +
                  " WHERE COF_NAME LIKE '" +  nombre +  "'";
           
-        
+        System.out.println(updateString);
         Connection con = PoolConexiones.getConexionLibre();
         if(con == null){
             throw new TiendaCafesException("Tienda. No se puede modificar ventas. No hay conexión db");
@@ -58,4 +59,28 @@ public class GestionarCafes {
         
     }//fin metodo
     
+     
+     public void vender(String nombreCafe, int cantidad) throws TiendaCafesException, SQLException{
+          String insertString =
+                 "INSERT INTO VENTAS (FECHA_VENTA, NOMBRE_CAFE, CANTIDAD) " +
+                 "VALUES ( ?, ?, ?)";
+
+        Connection con = PoolConexiones.getConexionLibre();
+        if(con == null){
+            throw new TiendaCafesException("Tienda. No se puede hacer la venta. No hay conexión db");
+        }
+        PreparedStatement st = con.prepareStatement(insertString);
+        java.util.Date hoy = new java.util.Date();
+        st.setDate(1, new java.sql.Date(hoy.getTime()));
+        st.setString(2, nombreCafe);
+        st.setInt(3, cantidad);
+        
+        int insertados = st.executeUpdate();
+        if (insertados == 0){
+            throw new TiendaCafesException(("Tienda. No insertó la venta ."));
+        }
+        PoolConexiones.liberaConexion(con);
+    }//fin vender
+     
+     
 }
